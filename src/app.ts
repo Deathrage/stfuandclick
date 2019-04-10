8cd6cb912851eea11ef47a49aae6d08965197634
Deathrage <lukasino97@gmail.com>
2019-04-10 11:38:46 +0200

    Heroku

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import router from './router/router';
 import { createConnection } from 'typeorm';
 import bodyParser = require('body-parser');
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const startExpress = () => {
 	const app = express();
